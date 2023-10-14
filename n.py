94a64c0a65b9c6fbf8e302111b4d6230d1ed1de2
42423521 <121691582+42423521@users.noreply.github.com>
2023-10-15 02:52:12 +0800

    aaa_4 中的 for

---
+++ n.py
@@ -0,0 +1,48 @@
+n=int(input())
+
+#s=int(input())
+
+s=2
+
+sum=1
+
+s_1=s
+
+
+
+
+#if 
+
+
+
+#for i in range(n+1):
+#    sum=sum*s_1
+
+
+
+
+for i in range(n):
+    sum=sum*s_1
+    #print(sum)
+
+
+
+
+
+
+#print(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(sum) #16 24 32 48
